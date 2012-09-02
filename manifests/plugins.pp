18c8992a1d4064c97698059da39f1e3d5886ba28
Alessandro Franceschi <al@lab42.it>
2012-09-02 12:21:23 +0200

    Plugins management for decommisioning

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -4,41 +4,50 @@ class mcollective::plugins {
 
   # PuppetLabs Plugins
   mcollective::plugin { 'process':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
     application    => 'pgrep.rb',
   }
 
   mcollective::plugin { 'iptables':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
   }
 
   mcollective::plugin { 'filemgr':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
   }
 
   mcollective::plugin { 'nettest':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
   }
 
   mcollective::plugin { 'service':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
   }
 
   mcollective::plugin { 'puppetd':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
   }
 
   mcollective::plugin { 'puppetca':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
     application    => false,
   }
 
   mcollective::plugin { 'puppetral':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
     application    => false,
   }
 
   mcollective::plugin { 'package':
+    ensure         => $mcollective::manage_package,
     install_client => $mcollective::install_client,
   }
 
