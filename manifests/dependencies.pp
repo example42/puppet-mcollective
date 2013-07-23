8f10c5bb5efcf0574bac706845bbf38c817ef043
Markus Burger <markus.burger@uni-ak.ac.at>
2013-07-23 08:41:07 +0200

    added the json gem to the dependencies
    Change-Id: Ia6a3d847e1e935bdf7bacc58496b3ba0c1655ba7

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -3,7 +3,7 @@ class mcollective::dependencies {
   include mcollective
 
   package {
-    [ 'net-ping', 'sys-proctable' ]:
+    [ 'net-ping', 'sys-proctable', 'json' ]:
       ensure    => $mcollective::manage_package,
       provider  => gem,
       notify    => $mcollective::manage_service_autorestart
