fda00e569f0913894bad5387ac99296cbe52d81b
Markus Burger <markus.burger@uni-ak.ac.at>
2013-08-21 14:11:47 +0200

    added support for cpu and memory limit monitoring
    Change-Id: Ie4fb62446d0f283b9137b1d76ed3adfb79705bbe

--- manifests/init.pp
+++ manifests/init.pp
@@ -150,6 +150,32 @@
 #   Can be defined also by the (top scope) variables $mcollective_monitor_target
 #   and $monitor_target
 #
+# [*monitor_cpu_below*]
+#   The CPU usage in %
+#   Can be defined also by the (top scope) variables $mcollective_monitor_cpu_below
+#   and $monitor_cpu_below
+#
+# [*monitor_cpu_every*]
+#   Monitoring interval in Seconds
+#   Can be defined also by the (top scope) variables $mcollective_monitor_cpu_every
+#   and $monitor_cpu_every
+#
+# [*monitor_cpu_times*]
+#   Can be defined also by the (top scope) variables $mcollective_monitor_cpu_times
+#   and $monitor_cpu_times
+#
+# [*monitor_memory_below*]
+#   Can be defined also by the (top scope) variables $mcollective_monitor_memory_below
+#   and $monitor_memory_below
+#
+# [*monitor_memory_every*]
+#   Can be defined also by the (top scope) variables $mcollective_monitor_memory_every
+#   and $monitor_memory_every
+#
+# [*monitor_memory_times*]
+#   Can be defined also by the (top scope) variables $mcollective_monitor_memory_times
+#   and $monitor_memory_times
+#
 # [*puppi*]
 #   Set to 'true' to enable creation of module data files that are used by puppi
 #   Can be defined also by the top scope variables $mcollective_puppi and $puppi
@@ -306,6 +332,12 @@ class mcollective (
   $monitor              = params_lookup( 'monitor' , 'global' ),
   $monitor_tool         = params_lookup( 'monitor_tool' , 'global' ),
   $monitor_target       = params_lookup( 'monitor_target' , 'global' ),
+  $monitor_cpu_below    = params_lookup( 'monitor_cpu_below' , 'global' ),
+  $monitor_cpu_every    = params_lookup( 'monitor_cpu_every' , 'global' ),
+  $monitor_cpu_times    = params_lookup( 'monitor_cpu_times' , 'global' ),
+  $monitor_memory_below = params_lookup( 'monitor_memory_below' , 'global' ),
+  $monitor_memory_every = params_lookup( 'monitor_memory_every' , 'global' ),
+  $monitor_memory_times = params_lookup( 'monitor_memory_times' , 'global' ),
   $puppi                = params_lookup( 'puppi' , 'global' ),
   $puppi_helper         = params_lookup( 'puppi_helper' , 'global' ),
   $firewall             = params_lookup( 'firewall' , 'global' ),
@@ -542,6 +574,12 @@ class mcollective (
       argument => $mcollective::process_args,
       tool     => $mcollective::monitor_tool,
       enable   => $mcollective::manage_monitor,
+      cpu_below    => $mcollective::monitor_cpu_below,
+      cpu_times    => $mcollective::monitor_cpu_times,
+      cpu_every    => $mcollective::monitor_cpu_every,
+      memory_below => $mcollective::monitor_memory_below,
+      memory_times => $mcollective::monitor_memory_times,
+      memory_every => $mcollective::monitor_memory_every,
     }
   }
 
