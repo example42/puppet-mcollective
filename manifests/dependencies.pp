4a50845f05d1e3f28fef2087b31f904de7b29c5e
Markus Burger <markus.burger@uni-ak.ac.at>
2013-08-23 12:50:21 +0200

    dont manage the semi-required gems if already specified
    Change-Id: I5a0d98baaac6ed0460f756fbc1fdf064e4aebbe3

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -2,10 +2,25 @@ class mcollective::dependencies {
 
   include mcollective
 
-  package {
-    [ 'net-ping', 'sys-proctable', 'json' ]:
+  if !defined(Package[json]) {
+    package { 'json':
       ensure    => $mcollective::manage_package,
       provider  => gem,
       notify    => $mcollective::manage_service_autorestart
+    }
+  }
+  if !defined(Package[net-ping]) {
+    package { 'net-ping':
+      ensure    => $mcollective::manage_package,
+      provider  => gem,
+      notify    => $mcollective::manage_service_autorestart
+    }
+  }
+  if !defined(Package[sys-proctable]) {
+    package { 'sys-proctable':
+      ensure    => $mcollective::manage_package,
+      provider  => gem,
+      notify    => $mcollective::manage_service_autorestart
+    }
   }
 }
