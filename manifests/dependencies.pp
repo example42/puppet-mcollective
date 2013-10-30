ef5d175ff6db0a88f36f656ee6de33d2e328bd77
Alessandro Franceschi <al@lab42.it>
2013-10-30 17:30:32 +0100

    net-ping package installed only on supported ruby versions

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -26,10 +26,12 @@ class mcollective::dependencies {
     }
   }
   if !defined(Package[net-ping]) {
-    package { 'net-ping':
-      ensure    => $mcollective::manage_package,
-      provider  => gem,
-      notify    => $mcollective::manage_service_autorestart
+    if $::rubyversion >= '1.9.1' {
+      package { 'net-ping':
+        ensure    => $mcollective::manage_package,
+        provider  => gem,
+        notify    => $mcollective::manage_service_autorestart
+      }
     }
   }
   if !defined(Package[sys-proctable]) {
